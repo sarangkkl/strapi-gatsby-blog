fe0ecb641d28f7d0e1efec9910000da1feb987ad
sarangkkl <sarangkkl2@gmail.com>
2022-06-18 15:07:23 +0530

    this is first commit

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,65 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
+        accessToken: process.env.STRAPI_TOKEN || "7c78b9c4db41de81fba504a52f3f4fb9ec95b7e4431207bd8c8eed74c6a6813be689457c4cfa3c6bea0e5f4b965cbf78f5fd788f7196a936a16c4b25ae50fc2de23aa0846b4c6106a890c9a905326992d39f0b5c7664ddffb8ad491d2f974e4f224463ec058b359371abff9bff71b4503f539df5bbe46fe0115660c30c458be8",
+        collectionTypes: [
+          {
+            singularName: "article",
+            queryParams: {
+              publicationState:
+                process.env.GATSBY_IS_PREVIEW === "true" ? "preview" : "live",
+              populate: {
+                cover: "*",
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "author",
+          },
+          {
+            singularName: "category",
+          },
+        ],
+        singleTypes: [
+          {
+            singularName: "about",
+            queryParams: {
+              populate: {
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "global",
+            queryParams: {
+              populate: {
+                favicon: "*",
+                defaultSeo: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
+  ],
+}
